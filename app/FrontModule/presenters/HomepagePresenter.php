8644e72ac5a6a4eb7e2352cb9353af01f8aeeabe
Mikuláš Dítě <rullaf@gmail.com>
2012-07-24 22:42:01 +0200

    add youtube player imitation to homepage

--- app/FrontModule/presenters/HomepagePresenter.php
+++ app/FrontModule/presenters/HomepagePresenter.php
@@ -10,6 +10,7 @@ class HomepagePresenter extends BaseFrontPresenter
 	public function renderDefault()
 	{
 		$this->template->categories = $this->context->categories->findRoot();
+		$this->template->featured_video = $this->context->videos->findAll()->order('Rand()')->limit(1)->fetch();
 	}
 
 }
