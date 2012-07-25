78e19f6d7d763be1dd7f9522adf1c8fd3bb9b4e4
Mikuláš Dítě <rullaf@gmail.com>
2012-07-25 09:48:38 +0200

    add function to populate db from translation, translate few exercises

--- app/ModeratorModule/presenters/DashboardPresenter.php
+++ app/ModeratorModule/presenters/DashboardPresenter.php
@@ -25,4 +25,48 @@ class DashboardPresenter extends BaseModeratorPresenter
 		$this->redirect(':Front:Exercise:edit', ['eid' => $exercise->id]);
 	}
 	
+	
+	
+	public function renderDetachedExercises()
+	{
+		$this->template->detached = $this->getDetachedExercises();
+	}
+	
+	
+	
+	public function handlePopulateDb()
+	{
+		foreach ($this->getDetachedExercises() as $file => $label) {
+			$this->context->exercises->insert([
+				'file' => $file,
+				'label' => $label,
+			]);
+		}
+		$this->redirect('this');
+	}
+	
+	
+	
+	private function getDetachedExercises()
+	{
+		$detached = [];
+		$path = WWW_DIR . "/exercise/translated";
+		
+		foreach (scandir($path) as $fullfile) {
+			if (in_array($fullfile, ['.', '..'])) {
+				continue;
+			}
+			
+			$file = substr($fullfile, 0, -5);
+			$exercise = $this->context->exercises->findOneBy(['file' => $file]);
+			if (!$exercise) {
+				$template = file_get_contents("$path/$fullfile");
+				$match = [];
+				preg_match('~(?<=<title>).*?(?=</title>)~', $template, $match);
+				$detached[$file] = $match[0];
+			}
+		}
+		
+		return $detached;
+	}
 }
