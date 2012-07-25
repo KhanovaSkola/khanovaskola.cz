34dc2a6a857fc82ad82a88e3e2eacfac9c2b0bd1
Mikuláš Dítě <rullaf@gmail.com>
2012-07-25 17:47:44 +0200

    add database dump

---
+++ database.sql
@@ -0,0 +1,61 @@
+-- Adminer 3.4.0-dev MySQL dump
+
+SET NAMES utf8;
+SET foreign_key_checks = 0;
+SET time_zone = 'SYSTEM';
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+USE `khanacademy`;
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` bigint(20) unsigned DEFAULT NULL,
+  `label` varchar(255) COLLATE utf8_czech_ci NOT NULL COMMENT 'titulek',
+  `description` text COLLATE utf8_czech_ci NOT NULL COMMENT 'abstrakt',
+  `position` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `label` (`label`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci COMMENT='Kategorie';
+
+
+DROP TABLE IF EXISTS `exercise`;
+CREATE TABLE `exercise` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `label` varchar(255) COLLATE utf8_czech_ci NOT NULL COMMENT 'název',
+  `file` varchar(255) COLLATE utf8_czech_ci NOT NULL COMMENT 'soubor',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `label` (`label`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci COMMENT='Cvičení';
+
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `role` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `mail` varchar(255) COLLATE utf8_czech_ci NOT NULL COMMENT 'mail',
+  `password` binary(32) NOT NULL,
+  `salt` binary(8) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci COMMENT='Uživatelé';
+
+
+DROP TABLE IF EXISTS `video`;
+CREATE TABLE `video` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `category_id` bigint(20) unsigned NOT NULL COMMENT 'kategorie',
+  `exercise_id` bigint(20) unsigned DEFAULT NULL COMMENT 'cvičení',
+  `label` varchar(255) COLLATE utf8_czech_ci NOT NULL COMMENT 'titulek',
+  `description` text COLLATE utf8_czech_ci NOT NULL COMMENT 'abstrakt',
+  `youtube_id` varchar(50) COLLATE utf8_czech_ci NOT NULL COMMENT 'youtube_id',
+  `position` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `label` (`label`),
+  KEY `category_id` (`category_id`),
+  KEY `exercise_id` (`exercise_id`),
+  CONSTRAINT `video_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
+  CONSTRAINT `video_ibfk_2` FOREIGN KEY (`exercise_id`) REFERENCES `exercise` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci COMMENT='Videa';
+
+
+-- 2012-07-25 17:47:23
