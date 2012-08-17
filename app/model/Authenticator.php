4ff7981a41a1ae533fa37f809e9b637109eaab67
Mikuláš Dítě <rullaf@gmail.com>
2012-08-17 16:02:51 +0200

    translate auth errors [Closes #19]

--- app/model/Authenticator.php
+++ app/model/Authenticator.php
@@ -33,12 +33,12 @@ class Authenticator extends Nette\Object implements NS\IAuthenticator
 		$user = $this->selector->findOneBy(['mail' => $mail]);
 
 		if (!$user) {
-			throw new NS\AuthenticationException("User '$mail' not found.", self::IDENTITY_NOT_FOUND);
+			throw new NS\AuthenticationException("Uživatel „{$mail}“ neexistuje.", self::IDENTITY_NOT_FOUND);
 		}
 
 		$hash = (new \Password())->calculateHash($password, $user->salt);
 		if ($user->password !== $hash) {
-			throw new NS\AuthenticationException("Invalid password.", self::INVALID_CREDENTIAL);
+			throw new NS\AuthenticationException("Nesprávné heslo.", self::INVALID_CREDENTIAL);
 		}
 
 		return new NS\Identity($user->id, explode(';', $user->role), []);
