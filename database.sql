e296285c4673b73fff5ba21aea0ba6d3a0a8f16a
Mikuláš Dítě <rullaf@gmail.com>
2012-08-19 12:29:55 +0200

    update database dump

--- database.sql
+++ database.sql
@@ -5,6 +5,21 @@ SET foreign_key_checks = 0;
 SET time_zone = 'SYSTEM';
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+DROP TABLE IF EXISTS `answer`;
+CREATE TABLE `answer` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `exercise_id` bigint(20) unsigned NOT NULL,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `correct` tinyint(1) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `exercise_id` (`exercise_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `answer_ibfk_1` FOREIGN KEY (`exercise_id`) REFERENCES `exercise` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `answer_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -98,6 +113,30 @@ CREATE TABLE `tag_video` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci COMMENT='Video + Tagy';
 
 
+DROP TABLE IF EXISTS `task`;
+CREATE TABLE `task` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `coach_id` bigint(20) unsigned NOT NULL,
+  `user_id` bigint(20) unsigned DEFAULT NULL COMMENT 'user_id OR group_id NOT NULL',
+  `group_id` bigint(20) unsigned DEFAULT NULL COMMENT 'user_id OR group_id NOT NULL',
+  `video_id` bigint(20) unsigned DEFAULT NULL COMMENT 'video_id OR exercise_id NOT NULL',
+  `exercise_id` bigint(20) unsigned DEFAULT NULL COMMENT 'video_id OR exercise_id NOT NULL',
+  `completed` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `deadline` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `group_id` (`group_id`),
+  KEY `video_id` (`video_id`),
+  KEY `exercise_id` (`exercise_id`),
+  KEY `coach_id` (`coach_id`),
+  CONSTRAINT `task_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `task_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `group` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `task_ibfk_3` FOREIGN KEY (`video_id`) REFERENCES `video` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `task_ibfk_4` FOREIGN KEY (`exercise_id`) REFERENCES `exercise` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `task_ibfk_5` FOREIGN KEY (`coach_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -132,4 +171,4 @@ CREATE TABLE `video` (
 DROP VIEW IF EXISTS `videos_with_error`;
 CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `videos_with_error` AS select `video`.`id` AS `id`,`video`.`category_id` AS `category_id`,`video`.`exercise_id` AS `exercise_id`,`video`.`label` AS `label`,`video`.`description` AS `description`,`video`.`youtube_id` AS `youtube_id`,`video`.`position` AS `position` from `video` where (`video`.`description` like '%.');
 
--- 2012-08-18 20:34:41
+-- 2012-08-19 12:29:41
