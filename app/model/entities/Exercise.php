9bbb992e2b53721b12a1ca17afd83ec1a21fe9d1
Mikuláš Dítě <rullaf@gmail.com>
2012-08-22 14:09:55 +0200

    improve exercise description

--- app/model/entities/Exercise.php
+++ app/model/entities/Exercise.php
@@ -13,4 +13,28 @@ class Exercise extends Entity
 		return $this->context->videos->findByExercise($this);
 	}
 
+
+
+	public function getDescription()
+	{
+		if ($this->getRelatedVideos()->count()) {
+			$desc = [];
+			$labels = [];
+			foreach ($this->getRelatedVideos() as $video) {
+				if ($video->description) {
+					$desc[] = $video->description;
+				}
+				$labels[] = $video->label;
+			}
+
+			if (count($desc)) {
+				return implode(' ', $desc);
+			} else {
+				return "Cvičení {$this->label} na Khanově škole pro lekc" . (count($labels) > 1 ? 'e ' : 'i ') . implode(', ', $labels);
+			}
+		}
+
+		return "Cvičení {$this->label} na Khanově škole.";
+	}
+
 }
