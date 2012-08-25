4013485aae403136ae4d788cefb0044a0c7f91c5
Mikuláš Dítě <rullaf@gmail.com>
2012-08-25 10:13:54 +0200

    replace inline code with youtube component

--- app/components/Youtube/Youtube.php
+++ app/components/Youtube/Youtube.php
@@ -5,7 +5,7 @@ class Youtube extends BaseControl
 
     public function render(Video $video, $autoplay = FALSE)
     {
-        $this->template->setFile(__DIR__ . '/tablet.latte');
+        $this->template->setFile(__DIR__ . '/template.latte');
         $this->template->video = $video;
         $this->template->autoplay = $autoplay;
         $this->template->render();
