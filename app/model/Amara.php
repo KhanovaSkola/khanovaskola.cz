bb50484dc0d94fa0bdc57214636805de00606df4
Mikulas <mikulas@dite.cz>
2012-11-18 21:23:10 +0100

    greatly improve subtitle load, remove srt parser

--- app/model/Amara.php
+++ app/model/Amara.php
@@ -30,59 +30,19 @@ class Amara extends Nette\Object
 
 
 
-	public function getVideoMeta(Video $video)
-	{
-		$url = "http://www.youtube.com/watch?v={$video->youtube_id}";
-		return $this->parseRequest(self::ENDPOINT . "/api2/partners/videos/?video_url=" . urlencode($url));
-	}
-
-
-
 	public function getSubtitles(Video $video)
 	{
 		$cache = new \Nette\Caching\Cache($this->cacheStorage, 'subtitles');
 		if (!isset($cache[$video->id])) {
-			$meta = $this->getVideoMeta($video);
-			if (!count($meta->objects)) {
-				return FALSE;
-			}
-
-			$id = $meta->objects[0]->id;
-			$content = $this->makeRequest(self::ENDPOINT . "/api2/partners/videos/$id/languages/cs/subtitles/?format=srt");
-			$res = SrtParser::parse($content);
-			$cache->save($video->id, $res);
+			$url = self::ENDPOINT . '/widget/rpc/jsonp/show_widget?video_url=' . urlencode("\"http://www.youtube.com/watch?v={$video->youtube_id}\"") . '&is_remote=true&base_state=%7B%22language%22%3A%22cs%22%7D&callback=';
+			$res = file_get_contents($url);
+			$data = \Nette\Utils\Json::decode(substr($res, 1, -2));
+			
+			$cache->save($video->id, $data->subtitles->subtitles);
+			return $data->subtitles->subtitles;
 		}
 
 		return $cache[$video->id];
-	}
-
-
-
-	protected function parseRequest($target)
-	{
-		return Json::decode($this->makeRequest($target));
-	}
-
-
-
-	protected function makeRequest($target)
-	{
-		$c = curl_init();
-		curl_setopt_array($c, [
-			CURLOPT_URL => $target,
-			CURLOPT_HTTPHEADER => [
-				"X-api-username: {$this->username}",
-				"X-apikey: {$this->key}",
-				"Accept: application/json",
-			],
-			CURLOPT_FOLLOWLOCATION => TRUE,
-			CURLOPT_RETURNTRANSFER => TRUE,
-			CURLOPT_SSL_VERIFYHOST => FALSE,
-		]);
-		$res = curl_exec($c);
-		curl_close($c);
-
-		return $res;
 	}
 
 }
