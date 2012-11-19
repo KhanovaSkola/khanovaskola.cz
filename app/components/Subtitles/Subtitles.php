6b9ec6d26b68f90694932137c51bb76d79aac015
Mikulas <mikulas@dite.cz>
2012-11-19 21:12:33 +0100

    add subtitle seek time

--- app/components/Subtitles/Subtitles.php
+++ app/components/Subtitles/Subtitles.php
@@ -24,4 +24,17 @@ class Subtitles extends BaseControl
 		return $amara->getSubtitles($video);
 	}
 
+
+
+	protected function createTemplate($class = NULL)
+	{
+		$template = parent::createTemplate($class);
+		$template->registerHelper('time', function ($sec) {
+			$minutes = floor($sec / 60);
+			$seconds = floor($sec % 60);
+			return str_pad($minutes, 2, '0', \STR_PAD_LEFT) . ':' . str_pad($seconds, 2, '0', \STR_PAD_LEFT);
+		});
+		return $template;
+	}
+
 }
