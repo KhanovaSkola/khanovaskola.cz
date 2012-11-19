88cc62faf2aabf85ac9abe1097c803ff895d464a
Mikulas <mikulas@dite.cz>
2012-11-19 21:45:15 +0100

    fix subtitle component

--- app/components/Subtitles/Subtitles.php
+++ app/components/Subtitles/Subtitles.php
@@ -19,8 +19,7 @@ class Subtitles extends BaseControl
 
 	private function getSubtitles(Video $video)
 	{
-		$par = $this->presenter->context->params['amara'];
-		$amara = new \Amara($par['key'], $par['username'], $this->presenter->context->cacheStorage);
+		$amara = new \Amara($this->presenter->context->cacheStorage);
 		return $amara->getSubtitles($video);
 	}
 
