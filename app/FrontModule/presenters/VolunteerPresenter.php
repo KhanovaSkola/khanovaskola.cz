33b64587d6efbfc7beb90822cafb79399cb82092
Mikulas <mikulas@dite.cz>
2012-12-07 10:08:57 +0100

    move volunteers to database

--- app/FrontModule/presenters/VolunteerPresenter.php
+++ app/FrontModule/presenters/VolunteerPresenter.php
@@ -6,4 +6,9 @@ namespace FrontModule;
 class VolunteerPresenter extends BaseFrontPresenter
 {
 
+	public function renderDefault()
+	{
+		$this->template->volunteers = $this->context->volunteers->findAll()->order('name ASC');
+	}
+
 }
---
+++ app/model/selectors/Volunteers.php
@@ -0,0 +1,6 @@
+<?php
+
+class Volunteers extends Table
+{
+
+}
