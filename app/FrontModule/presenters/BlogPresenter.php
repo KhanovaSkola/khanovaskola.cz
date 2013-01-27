94ca807baa7d9dc334cdab68ef80e878902dbc28
Mikulas <rullaf@gmail.com>
2013-01-27 17:09:53 +0100

    fix blog rss

--- app/FrontModule/presenters/BlogPresenter.php
+++ app/FrontModule/presenters/BlogPresenter.php
@@ -177,7 +177,7 @@ class BlogPresenter extends BaseFrontPresenter
 			$items[] = [
 				'link' => $this->link("//:Front:Blog:detail", ['aid' => $article->id]),
 				'title' => $article->label,
-				'description' => \Markdown($article->text),
+				'description' => \Michelf\Markdown::defaultTransform($article->text),
 			];
 		}
 
