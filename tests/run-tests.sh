eeb53a038d8b1c9d53356febdd87cfde0d355e56
Mikulas <rullaf@gmail.com>
2013-01-28 15:07:52 +0100

    fix run-tests script not dropping test database

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -4,4 +4,4 @@ echo Migrate database
 php db/migrate.php test --drop > /dev/null
 php db/migrate.php test > /dev/null
 php vendor/nette/tester/Tester/tester tests/ -s
-#php db/migrate.php test --drop > /dev/null
+php db/migrate.php test --drop > /dev/null
