da3b1873bab3f80d74d11ec59323c29724e8a478
Mikulas <rullaf@gmail.com>
2013-01-29 19:55:12 +0100

    improve volunteer page caching [Updates #164]

--- app/FrontModule/presenters/VolunteerPresenter.php
+++ app/FrontModule/presenters/VolunteerPresenter.php
@@ -2,23 +2,18 @@
 
 namespace FrontModule;
 
+use \Nette\Caching\Cache;
+
 
 class VolunteerPresenter extends BaseFrontPresenter
 {
 
 	public function renderDefault()
 	{
+		$this->template->eng_videos = 3600;
+
 		$this->template->volunteers = $this->context->volunteers->findAll()->order('name ASC');
-
-		$this->template->wanted_cats = $this->context->categories->findByVotes();
-
-		$dubbed = $this->context->videos->findAllDubbed()->count();
-		$translated = $this->context->videos->findAll()->count();
-		$this->template->count = (object) [
-			'Zbývá' => 3600 - $translated,
-			'Přeložené' => $translated - $dubbed,
-			'Dabované' => $dubbed,
-		];
+		$this->template->wanted_cats =$this->context->categories->findByVotes();
 	}
 
 }
