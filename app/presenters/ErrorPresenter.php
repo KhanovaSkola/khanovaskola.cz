24339378e4e0ffbbb35464afcba158a0b2cbe001
Mikulas <rullaf@gmail.com>
2013-02-19 19:17:32 +0100

    refactor error pages, add js back button

--- app/presenters/ErrorPresenter.php
+++ app/presenters/ErrorPresenter.php
@@ -21,14 +21,18 @@ class ErrorPresenter extends BasePresenter
 			$this->terminate();
 
 		} elseif ($exception instanceof NA\BadRequestException) {
-			$code = $exception->getCode();
-			// load template 403.latte or 404.latte or ... 4xx.latte
-			$this->setView(in_array($code, [403, 404, 405, 410, 500]) ? $code : '4xx');
+			$c = $exception->getCode();
+			//$this->setView('error');
+			$code = in_array($c, [403, 404, 405, 410, 500]) ? $c : '4xx';
+			$this->template->code = $code;
+			$this->setView($c);
+
 			// log to access.log
 			Debugger::log("HTTP code $code: {$exception->getMessage()} in {$exception->getFile()}:{$exception->getLine()}", 'access');
 
 		} else {
-			$this->setView('500'); // load template 500.latte
+			$this->setView('error');
+			$this->template->code = 500;
 			Debugger::log($exception, Debugger::ERROR); // and log exception
 		}
 	}
