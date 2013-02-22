34948397b4e32eef6b4908e99d6ce253e5a338a0
Mikulas <rullaf@gmail.com>
2013-02-22 10:00:16 +0100

    disable post-stats cron debug mode

--- app/post-stats.php
+++ app/post-stats.php
@@ -9,10 +9,6 @@ use Metrics\Client;
 // Configure application
 $configurator = new Nette\Config\Configurator;
 
-// Enable Nette Debugger for error visualisation & logging
-$configurator->setDebugMode(TRUE);
-$configurator->enableDebugger(__DIR__ . '/../log');
-
 // Enable RobotLoader - this will load all classes automatically
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 $configurator->createRobotLoader()
