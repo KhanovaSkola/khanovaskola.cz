64cd8bf9f15422d74dbf343386c45ebe1b252d92
Mikulas <rullaf@gmail.com>
2013-03-23 15:27:18 +0100

    add /p for form

--- app/config/Routes.php
+++ app/config/Routes.php
@@ -13,6 +13,8 @@ class Routes
 	{
 		$container->router[] = new Route('index.php', 'Front:Homepage:default', Route::ONE_WAY);
 
+		$container->router[] = new Route('p', 'Front:Homepage:preklad', Route::ONE_WAY);
+		
 		/** OLD GONE ROUTE */
 		$container->router[] = new Route('vsechny-problemy', [
 			'module' => 'Front',
