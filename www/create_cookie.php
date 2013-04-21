1b3749fd53c7f0c00621d475641a32f21e1d8fcb
Mikulas <rullaf@gmail.com>
2013-04-21 19:01:25 +0200

    update staging cookie creator

--- www/create_cookie.php
+++ www/create_cookie.php
@@ -1,7 +1,8 @@
 <?php
 
 // I know it's de-facto public, it's mainly targeting web robots and search engines
-if (isset($_GET['auth']) && $_GET['auth'] === '6gY[]TzwR(9rXw') {
-        setcookie('staging_access', 'allowed');
-} 
-header("Location:http://staging.khanovaskola.cz/");
+if (isset($_GET['auth']) && $_GET['auth'] === '8gG[]TzwR(9rXw') {
+    setcookie('staging_access', 'allowed');
+}
+
+header("Location:http://new.khanovaskola.cz/");
