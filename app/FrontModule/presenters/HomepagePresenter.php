3ad565b91479a0e8aecee408cb920066c8d96c26
Mikulas <rullaf@gmail.com>
2013-04-24 17:04:25 +0200

    fix redirect

--- app/FrontModule/presenters/HomepagePresenter.php
+++ app/FrontModule/presenters/HomepagePresenter.php
@@ -11,7 +11,7 @@ class HomepagePresenter extends BaseFrontPresenter
 
 	public function renderDefault()
 	{
-		$this->redirectUrl("http://new.khanovaskola.cz/create_cookie.php?auth=8gG[]TzwR(9rXw");
+		//$this->redirectUrl("http://new.khanovaskola.cz/create_cookie.php?auth=8gG[]TzwR(9rXw");
 
 		$this->template->featured_video = $this->context->videos->findRandomDubbed();
 		$this->template->hide_search = TRUE;
