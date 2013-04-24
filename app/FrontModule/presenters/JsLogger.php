4582bca05d2ad6d3e2be5a09532e17a1bea563ad
Mikulas <rullaf@gmail.com>
2013-04-24 17:07:26 +0200

    make js log more readable

--- app/FrontModule/presenters/JsLogger.php
+++ app/FrontModule/presenters/JsLogger.php
@@ -8,7 +8,8 @@ class JsLoggerPresenter extends BaseFrontPresenter
 
 	public function actionDefault($description, $line, $parent_url, $user_agent, $user_id)
 	{
-		$content = "$description on line $line | $parent_url | $user_agent | user_id $user_id\n";
+		$user = $user_id ? $user_id : "n/a";
+		$content = date("Y-m-d H:i:s") . " [user $user] $description on line $line\n\t$parent_url\n\t$user_agent\n";
 		file_put_contents(WWW_DIR . '/../log/js_errors.log', $content, FILE_APPEND);
 		$this->terminate();
 	}
